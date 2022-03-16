4ab2a013d22f1bfd82d088a3baf072e7b4823ebd
Camille Morin <camille.morin.11@ulaval.ca>
2022-03-16 15:30:12 -0400

    comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from IdentificateurCodeBarre import base_de_donnees, decodage, verificationPresence, validationDernierChiffre
 
 dico = {}
-binaire = "1111111001100011"+"010"+ 6*"0001101" + "01010" + 6*"0001101" + "010"+ "000011010"
+binaire = "1111111001100011"+"010"+ 6*"0001101" + "01010" + 6*"0001101" + "010"+ "000011010" # à enlever plus tard (test)
 
 # Simple test pour vérifier que les fonctions fonctionnent bien
 if __name__ == "__main__":
