f9199bf46a67c7eb8fcb594c8d6acc6ed4cf6b53
Camille Morin <camille.morin.11@ulaval.ca>
2022-03-17 17:40:35 -0400

    decode

--- DecodePyZbar.py
+++ DecodePyZbar.py
@@ -1,31 +1,38 @@
+import pyzbar.pyzbar as pyzbar
 import matplotlib.pyplot as plt
 import numpy as np
 
+def convertToBytes(string01):
+    b = bytearray(b'')
+    im_array = bytearray(b'')
+    for i in string01:
+        if i == '1':
+            b.extend(b'\x00')
+        if i == '0':
+            b.extend(b'\xff')
+    for i in range(50):
+        im_array.extend(b)
+    return(im_array)
+
+
+def show(im_code, string01):
+    code = np.array([int(i) for i in im_code]).reshape(50, len(string01))
+    plt.imshow(code, 'gray')
+    plt.show()
+
+def decode(tuple):
+    # Trouver les code-barres dans l'image
+    codes_trouves = pyzbar.decode(tuple)
+    return(codes_trouves)
+
 #string = "010"+"011101100011010110001010111101111010010011"+"01010"+"111001010010001001110111010010111001000010"+"010"
-string = "10100011010010011001001100011010001101000110101010110011011011001001110111001010000101000010101"
-b = bytearray(b'')
-im_array = bytearray(b'')
-for i in string:
-    if i == '1':
-        b.extend(b'\xff')
-    if i == '0':
-        b.extend(b'\x00')
-for i in range(20):
-    im_array.extend(b)
+string = "0000000000000000000000000000000001100110000001111001100001111000011000011000011110011111111001100110000001111001111000000110011001100110011000000001100000011000011000011000000111111001100001111000011110011111100001100110011000000000000000000000000"
 
-code = np.array([int(i) for i in im_array]).reshape(20, len(string))
-plt.imshow(code, 'gray')
-plt.show()
-
-# bit = "1111111001100011"+"010"+ 6*"0001101" + "01010" + 6*"0001101" + "010"+ "000011010"
-# im_array = np.array([])
-
-# for i in 
-
-# for i in bit:
-#     im_array.append()
-
-# im_array = np.array([int(i) for i in bit])
-# bite_array = bytearray((bit))
-
-# print(bite_array)
+string8 = convertToBytes(string)
+#print(string8)
+show(string8, string)
+myTuple = (string8, 0, 0)
+codes = decode(myTuple)
+for obj in codes:
+    print('Type : ', obj.type)
+    print('Data : ', obj.data,'\n')
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from IdentificateurCodeBarre import base_de_donnees, decodage, verificationPresence, validationDernierChiffre
+from facture import Facture
+
+
+binaire = "1111111001100011"+"010"+ 6*"0001101" + "01010" + 6*"0001101" + "010"+ "000011010" # à enlever plus tard (test)
 
 dico = {}
-binaire = "1111111001100011"+"010"+ 6*"0001101" + "01010" + 6*"0001101" + "010"+ "000011010" # à enlever plus tard (test)
+facture = {}
 
 # Simple test pour vérifier que les fonctions fonctionnent bien
 if __name__ == "__main__":
+    facture = Facture.facture()
     dico = base_de_donnees(dico)
     Code = decodage(binaire)
     print(Code)
