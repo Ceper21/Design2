0fc2c13fc44f832e37013e4480bc7a23fcb35d1e
Alex Dallaire <alex.dallaire.2@ulaval.ca>
2022-03-17 17:42:44 -0400

    interface2

--- Interface.py
+++ Interface.py
@@ -3,13 +3,17 @@
 from tkinter import *
 from tkinter import ttk, font
 from turtle import right
+#from facture import Facture
+
 
 contour = Tk()
+contour.title('Identification de code-barres')
 contour.geometry('1000x1000')
+
 
 prix = "PRIX TOTAL DE LA COMMANDE"
 
-total = Label(contour, text='Total : '+prix).place(anchor = SW)
+total = Label(contour, text='Total : '+prix).pack(side = BOTTOM)
 
 table = Frame(contour)
 table['bg'] = '#AC99F2'
@@ -21,14 +25,13 @@ tablescroll.pack(side=RIGHT, fill=Y)
 matable = ttk.Treeview(table, yscrollcommand=tablescroll.set)
 tablescroll.config(command=matable.yview)
 
-
 matable['columns'] = ('nom', 'qte', 'prix', 'prixtot')
 
 matable.column("#0", width=0,  stretch=NO)
-matable.column("nom", anchor=CENTER, width=80)
-matable.column("qte",anchor=CENTER, width=80)
-matable.column("prix",anchor=CENTER,width=80)
-matable.column("prixtot",anchor=CENTER,width=100)
+matable.column("nom", anchor=CENTER, width=200)
+matable.column("qte",anchor=CENTER, width=200)
+matable.column("prix",anchor=CENTER,width=200)
+matable.column("prixtot",anchor=CENTER,width=200)
 
 matable.heading("#0",text="",anchor=CENTER)
 matable.heading("nom",text="Article",anchor=CENTER)
@@ -45,8 +48,8 @@ values=('3','Deamon','103', 'California', 'Placentia'))
 matable.insert(parent='',index='end',iid=3,text='',
 values=('4','Dragon','104','New York' , 'White Plains'))
 
-matable.pack()
 
+matable.pack()
 f2 = font.Font(family='Helvetica', size=20, weight="bold")
 
 blackbutton = Button(contour, text="Imprimer le reçu",fg="black", command=contour.quit, height = 2, width = 20) #Mettre le calcul de sommation en commande + affichage de la facture
