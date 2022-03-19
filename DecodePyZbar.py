84894987b043f65f6fdc81617460167c4fa1e4d7
Camille Morin <camille.morin.11@ulaval.ca>
2022-03-19 12:23:50 -0400

    plus clean

--- DecodePyZbar.py
+++ DecodePyZbar.py
@@ -35,19 +35,17 @@ def flipIt(stringin):
             stringout += '1'
     return(stringout)
 
+# code pour tester 
+# string = '00000000000010101110110001001010011100010110000101001100101010100001011101001101100110011010111001000100101000000000000000'
 
-#string = "0000000000000000000000000000000001100110000001111001100001111000011000011000011110011111111001100110000001111001111000000110011001100110011000000001100000011000011000011000000111111001100001111000011110011111100001100110011000000000000000000000000"
-#string = '10101101110110001000101101000110100011000110101111010101011001101100110111001011001101100110101'
-string = '00000000000010101110110001001010011100010110000101001100101010100001011101001101100110011010111001000100101000000000000000'
-
-stringNotFlip = flipIt(string)
-string8 = convertToBytes(stringNotFlip)
-print(type(string8))
-print(type(len(string)))
-print(type(hauteur))
-show(string8, string)
-myTuple = (string8, len(string), hauteur)
-codes = decoder(myTuple)
-for obj in codes:
-    print('Type : ', obj.type)
-    print('Data : ', obj.data,'\n')
+# stringNotFlip = flipIt(string)
+# string8 = convertToBytes(stringNotFlip)
+# print(type(string8))
+# print(type(len(string)))
+# print(type(hauteur))
+# show(string8, string)
+# myTuple = (string8, len(string), hauteur)
+# codes = decoder(myTuple)
+# for obj in codes:
+#     print('Type : ', obj.type)
+#     print('Data : ', obj.data,'\n')
