9c64003913610156d6722d7eba19142ceffdaf27
Charles-Anthony <chthony84@gmail.com>
2022-03-19 12:42:26 -0400

    main_modif_test

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+import serial 
+import time 
+from DecodePyZbar import decoder
 from IdentificateurCodeBarre import base_de_donnees, decodage, verificationPresence, validationDernierChiffre
 from facture import Facture
 
 
-binaire = "1111111001100011"+"010"+ 6*"0001101" + "01010" + 6*"0001101" + "010"+ "000011010" # à enlever plus tard (test)
+#binaire = "1111111001100011"+"010"+ 6*"0001101" + "01010" + 6*"0001101" + "010"+ "000011010" # à enlever plus tard (test)
 
-dico = {}
+#dico = {}
 
 # Simple test pour vérifier que les fonctions fonctionnent bien
 if __name__ == "__main__":
-    facture = Facture.facture()
-    dico = base_de_donnees(dico)
-    Code = decodage(binaire)
-    print(Code)
-    print(verificationPresence(Code, dico))
-    x = dico["000000000000"]
-    print(x)
+
+    ser = serial.Serial('COM5', 9600, timeout=1)
+    time.sleep(2)
+
+    for i in range(50):
+        photodiode_value = ser.readline()    #read a byte
+        if photodiode_value:
+            string = photodiode_value.decode()  #Converting the byte string to a unicode string
+            real_string = string.strip()
+            print(real_string)
+    
+    
+
+    
+    ser.close()
+
+    #facture = Facture.facture()
+    #dico = base_de_donnees(dico)
+    #Code = decodage(binaire)
+    #print(Code)
+    #print(verificationPresence(Code, dico))
+    #x = dico["000000000000"]
+    #print(x)
